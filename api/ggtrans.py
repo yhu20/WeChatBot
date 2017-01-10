c270261dddd11e51cfd515fcdc4609b2b17c29be
Yang Hu <Yang Hu@MCKINSEY.COM>
2017-01-10 15:20:45 -0500

    Added a simple translator service, which could support translate other language to Chinese.

---
+++ api/ggtrans.py
@@ -0,0 +1,39 @@
+from langdetect import detect
+from translate import Translator
+
+
+ch_language_code = {u'繁体中文': 'zh-TW',
+                    u'中文': 'zh',
+                    u'简体中文': 'zh',
+                    u'英语': 'en',
+                    u'日语': 'ja',
+                    u'法语': 'fr'}
+
+
+def translate2(msg, targetl='zh'):
+    """
+    Translate from one language to another language. If sourcel is the same as targetl, then tranlate it to ch-TW.
+    :param targetl: language abbreviation.
+    :param msg: message.
+    :return: message in another language.
+    """
+    sourcel = detect(msg)
+    if detect(targetl) in ('zh', 'zh-TW', 'zh-cn'):
+        try:
+            targetl_ = ch_language_code[targetl] if targetl != sourcel else 'zh-TW'
+        except:
+            return 'No this language available!'
+    else:
+        # print(detect(targetl))  #DEBUG
+        targetl_ = targetl if targetl != sourcel else 'zh-TW'
+
+    translator = Translator(to_lang=targetl_)
+    translation = translator.translate(msg)
+    return translation
+
+
+
+
+if __name__ == '__main__':
+
+    print(translate2(u'翻译 what is this? Can you understand me?', u'中文'))
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import re
 import sys
 sys.path.append("ItChat-master")
 
@@ -14,8 +15,17 @@ def text_reply(msg):
     elif u'源代码' in msg['Text'] or u'获取文件' in msg['Text']:
         itchat.send('@fil@main.py', msg['FromUserName'])
         return u'这就是现在机器人后台的代码，是不是很简单呢？'
-    elif u'获取图片' in msg['Text']:
-        itchat.send('@img@applaud.gif', msg['FromUserName']) # there should be a picture
+    elif u'获取图片' in msg['Text'] or u'自拍' in msg['Text']:
+        itchat.send('@img@irobot.jpg', msg['FromUserName']) # there should be a picture
+    elif u'翻译' or u'translate' in msg['Text']:
+        # print(msg['Text'])
+        raw_text = re.split(':|：', msg['Text'], 1)
+        targetl = raw_text[0]
+        text = raw_text[-1]
+
+        from api import ggtrans
+
+        return ggtrans.translate2(text)
     else:
         return get_response(msg['Text']) or u'收到：' + msg['Text']
 
@@ -43,8 +53,24 @@ def mm_reply(msg):
 def group_reply(msg):
     if msg['isAt']:
         text = msg['Text'].split(' ')[1].strip()
-        return u'@%s\u2005%s' % (msg['ActualNickName'],
-            get_response(text) or u'收到：' + msg['Text'])
+        if u'作者' in text or u'主人' in text:
+            return u'我主人超级帅的！'
+        elif u'源代码' in text or u'获取文件' in text:
+            itchat.send('@fil@main.py', msg['FromUserName'])
+            return u'这就是现在机器人后台的代码，是不是很简单呢？'
+        elif u'获取图片' in text or u'自拍' in text:
+            itchat.send('@img@irobot.jpg', msg['FromUserName'])  # there should be a picture
+            return u'这就是帅气的我！'
+        elif u'翻译' or u'translate' in msg['Text']:
+            # print(msg['Text'])
+            raw_text = re.split(':|：', msg['Text'], 1)
+            targetl = raw_text[0]
+            text = raw_text[-1]
+            from api import ggtrans
+            return ggtrans.translate2(text)
+        else:
+            return u'@%s\u2005%s' % (msg['ActualNickName'],
+                get_response(text) or u'收到：' + msg['Text'])
 
 
 @itchat.msg_register('Friends')
